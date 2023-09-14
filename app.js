772260d261f13d118607e45c4411ca2a8a83dea2
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-09-14 20:02:39 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// TASK-1
+
+// alerts("Hello user")
+
+
+// task-2
+
+// alert("Error! pleease enter a valid password")
+
+
+// task-3
+// alert("Welcome to js Land...\nHappy Coding!")
+
+
+// task-4
+// console.log(alert("hello..i can run js through my web browser console"))
+
+// completed
+
+alert("hello")
